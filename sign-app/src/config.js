64120c03f1a27d26aea734eae2c9decde1fe6210
hudaibiao-1 <hudaibiao-1@thunisoft.com>
2020-09-28 17:41:58 +0800

    修改logo

--- sign-app/src/config.js
+++ sign-app/src/config.js
@@ -1,6 +1,6 @@
 const url = {
-    server_url:'http://www.zy-ht.cn:8080/',
-    client_url:'http://www.zy-ht.cn/',
+    server_url:'http://www.cdqsx.vip:8080/',
+    client_url:'http://www.cdqsx.vip/',
     // server_url:'http://192.168.1.101:8080/',
     // client_url:'http://192.168.1.101:80/',
 };
